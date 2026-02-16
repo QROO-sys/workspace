bc89768dbca73774ef0a00226f0481a80eab1de8
QROO-sys <mohammed@qr-oo.com>
2026-02-16 17:32:50 +0200

    Delete src directory

--- src/public/public.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PublicController } from './public.controller';
-import { PrismaService } from '../prisma.service';
-
-@Module({
-  controllers: [PublicController],
-  providers: [PrismaService],
-})
-export class PublicModule {}
